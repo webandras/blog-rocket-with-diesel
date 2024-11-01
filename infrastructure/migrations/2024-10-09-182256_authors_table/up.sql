e4b9a3347af63504b93c51a1f2f9011c24e942c0
Gulácsi András <guland@protonmail.com>
2024-11-01 15:24:07 +0100

    Add dummy data to tables

--- infrastructure/migrations/2024-10-09-182256_authors_table/up.sql
+++ infrastructure/migrations/2024-10-09-182256_authors_table/up.sql
@@ -6,4 +6,10 @@ CREATE TABLE authors (
   is_active BOOLEAN NOT NULL DEFAULT true
 );
 
--- ALTER TABLE authors ADD CONSTRAINT unique_email UNIQUE (email);
+ALTER TABLE authors
+ADD CONSTRAINT unique_email UNIQUE (email);
+
+INSERT INTO authors (firstname, lastname, email, is_active) VALUES
+    ('Any', 'More', 'anymore@example.com', TRUE),
+    ('Elek', 'Teszt', 'teszt@elek.hu', TRUE),
+    ('Tina', 'Klemen', 'klemen@tina.hu', TRUE);
---
+++ infrastructure/migrations/2024-11-01-141534_add_dummy_data_to_posts/up.sql
@@ -0,0 +1,23 @@
+INSERT INTO posts (title, body, genre, published, author_id) VALUES
+    (
+        'Megint elhalasztotta új kémfunkciója bevezetését a Windows 11-be a Microsoft',
+        'A Microsoft - miután idén már egyszer visszavonta, majd elhalasztotta - a héten ismét eltolta a Windows 11-be szánt kémfunkciója bevezetését.',
+        'Hírek',
+        TRUE,
+        1
+    ),
+    (
+        'A Google-nél már az új kódok 25%-át a mesterséges intelligencia írja',
+        'Bár a Google hagyományosan a mesterséges intelligenciák fejlesztése és elterjesztése egyik legnagyobb úttörőjének számít, ami - a Microsoft-hoz hasonlóan - minden termékébe megpróbálja betuszkolni az új divattechnológiát, eddig nem volt egyértelmű, hogy saját maga belső folyamataiban mennyire épít erre.',
+        'Hírek',
+        TRUE,
+        2
+    ),
+    (
+        'Sajátmárkás SSD-meghajtókat jelentett be a Raspberry Pi',
+        'Alig két héttel az után, hogy saját SD-kártyákat kezdett el kínálni miniszámítógépeihez, a Raspberry Pi újabb flash-alapú tárolóegységek kiadását jelentette be.',
+        'Hírek',
+        TRUE,
+        3
+    );
+
